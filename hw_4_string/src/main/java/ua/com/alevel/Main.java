0208c93695d6d3fd4319b9d8e5e502f99c1acaa1
egorfuntusov <funtushan@gmail.com>
2023-03-14 10:06:48 +0200

    add feedback

--- hw_4_string/src/main/java/ua/com/alevel/Main.java
+++ hw_4_string/src/main/java/ua/com/alevel/Main.java
@@ -3,6 +3,7 @@ package ua.com.alevel;
 import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Controller controller = new Controller();
         controller.start();
